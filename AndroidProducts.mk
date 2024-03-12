82a842b00b21abdad836b22a77deaeea7c75c66b
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-12 10:25:12 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2020 The superiorOS Project
+# Copyright (C) 2020 The risingOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_X01BD.mk
+    $(LOCAL_DIR)/rising_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_X01BD-userdebug
+    rising_X01BD-userdebug
