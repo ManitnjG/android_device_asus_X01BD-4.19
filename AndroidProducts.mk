1d8292879096e0064268bdf2221cb6ae553330d8
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-14 20:42:13 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The risingOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X01BD.mk
+    $(LOCAL_DIR)/rising_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-userdebug
+    rising_X01BD-userdebug
