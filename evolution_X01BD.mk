6e3a780e2e4a914c4efe8b066e6a57a7da755920
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-01 21:48:06 +0530

    Update and rename lineage_X01BD.mk to evolution_X01BD.mk

--- evolution_X01BD.mk
+++ evolution_X01BD.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The evolutionOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common evolutionOS stuff
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -19,7 +19,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 MATRIXX_MAINTAINER := ManitnjG
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X01BD
+PRODUCT_NAME := evolution_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M2
