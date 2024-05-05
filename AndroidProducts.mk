860ee871393bd6743ed4aaa48c9e8bf5b05183de
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-05 08:49:48 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2020 The lineageOS Project
+# Copyright (C) 2020 The voltageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X01BD.mk
+    $(LOCAL_DIR)/voltage_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-ap1a-userdebug
+    voltage_X01BD-userdebug
